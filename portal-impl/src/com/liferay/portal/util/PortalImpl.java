8b0d9657cd7744bd73d15948bc2a088f0763dfc9
Hugo Huijser <hugo.huijser@liferay.com>
2012-03-06 10:56:44 -0800

    Source formatting - missing parentheses in if statements

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -1549,7 +1549,7 @@ public class PortalImpl implements Portal {
 
 			Date date = cal.getTime();
 
-			/*if (timeZone != null &&
+			/*if ((timeZone != null) &&
 				cal.before(CalendarFactoryUtil.getCalendar(timeZone))) {
 
 				throw pe;
