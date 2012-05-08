08e3f79d910feb1c3db5c99bf608529ece55daeb
Brian Chan <brian.chan@liferay.com>
2012-05-08 10:08:30 -0700

    LPS-27117 Source formatting

--- portal-impl/src/com/liferay/portlet/documentlibrary/lar/DLPortletDataHandlerImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/lar/DLPortletDataHandlerImpl.java
@@ -308,6 +308,7 @@ public class DLPortletDataHandlerImpl extends BasePortletDataHandler {
 
 		String titleWithExtension = DLUtil.getTitleWithExtension(fileEntry);
 		String extension = fileEntry.getExtension();
+
 		String dotExtension = StringPool.PERIOD + extension;
 
 		if (portletDataContext.isDataStrategyMirror()) {
