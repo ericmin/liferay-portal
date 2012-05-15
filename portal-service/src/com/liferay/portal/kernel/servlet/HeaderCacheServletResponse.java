0e384229b43dddb53ac68fbfc7126ecd680c1116
Igor Spasic <igor.spasic@liferay.com>
2012-05-15 08:02:10 +0200

    LPS-27112 sometimes causes NPE under Websphere

--- portal-service/src/com/liferay/portal/kernel/servlet/HeaderCacheServletResponse.java
+++ portal-service/src/com/liferay/portal/kernel/servlet/HeaderCacheServletResponse.java
@@ -196,9 +196,11 @@ public class HeaderCacheServletResponse extends HttpServletResponseWrapper {
 
 	@Override
 	public void setContentType(String contentType) {
-		_contentType = contentType;
+		if (contentType != null) {
+			_contentType = contentType;
 
-		super.setContentType(contentType);
+			super.setContentType(contentType);
+		}
 	}
 
 	@Override
