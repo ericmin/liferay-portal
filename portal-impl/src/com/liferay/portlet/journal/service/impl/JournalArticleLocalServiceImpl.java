8e9d3a8a7996b1c45ab11cd3bf7122c1c724318d
Sergio Sanchez <sergiosanchez@liferay.com>
2012-05-21 10:08:25 +0200

    LPS-27427 ExpandoValue is not deleted when a JournalArticle is deleted

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -671,7 +671,7 @@ public class JournalArticleLocalServiceImpl
 			// Expando
 
 			expandoValueLocalService.deleteValues(
-				JournalArticle.class.getName(), article.getResourcePrimKey());
+				JournalArticle.class.getName(), article.getId());
 
 			// Resources
 
