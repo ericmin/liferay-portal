f0d43f1d16c3f0ab6b31b0c8ac6d12b5fbf84c7f
Hugo Huijser <hugo.huijser@liferay.com>
2012-05-25 10:56:00 -0700

    LPS-26791 Source formatting

--- portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
+++ portal-impl/src/com/liferay/portal/lar/PortletDataContextImpl.java
@@ -495,8 +495,8 @@ public class PortletDataContextImpl implements PortletDataContext {
 		}
 		else if (PropsValues.PERMISSIONS_USER_CHECK_ALGORITHM == 6) {
 			Map<Long, Set<String>> roleIdsToActionIds = getActionIds_6(
-				_companyId, roleIds.getArray(), resourceName,
-				resourcePK, actionIds);
+				_companyId, roleIds.getArray(), resourceName, resourcePK,
+				actionIds);
 
 			for (Map.Entry<Long, String> entry : roleIdsToNames.entrySet()) {
 				long roleId = entry.getKey();
