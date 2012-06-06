6ce7500b032f9bbeeb45d058e019f8413e8c3bd2
Eric Chin <eric.chin@liferay.com>
2012-06-06 15:54:44 -0700

    LPS-27691 Updated start and end numbers with the correct parameter for pagination, and set the correct number of entries to the total

--- portal-service/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-service/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -919,7 +919,7 @@ public abstract class BaseIndexer implements Indexer {
 		int length = docs.size();
 
 		if (hasMore) {
-			length = length + (end - start);
+			length = documents.length;
 		}
 
 		hits.setLength(length);
