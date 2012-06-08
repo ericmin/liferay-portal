e8e3b80393c6ba8b4e7be536c19af26435ed52c8
Julio Camarero <julio.camarero@liferay.com>
2012-06-08 14:43:18 +0200

    LPS-27138 Pages Deleted in Site Template are still present in Sites

--- portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
+++ portal-impl/src/com/liferay/portal/lar/LayoutImporter.java
@@ -373,6 +373,9 @@ public class LayoutImporter {
 
 		// Layout and layout set prototype
 
+		String layoutSetPrototypeUuid = layoutsElement.attributeValue(
+			"layout-set-prototype-uuid");
+
 		if (group.isLayoutPrototype() && larType.equals("layout-prototype")) {
 			LayoutPrototype layoutPrototype =
 				LayoutPrototypeLocalServiceUtil.getLayoutPrototype(
@@ -407,31 +410,32 @@ public class LayoutImporter {
 				LayoutSetPrototypeLocalServiceUtil.getLayoutSetPrototype(
 					group.getClassPK());
 
-			String layoutSetPrototypeUuid = GetterUtil.getString(
+			String importedLayoutSetPrototypeUuid = GetterUtil.getString(
 				headerElement.attributeValue("type-uuid"));
 
 			LayoutSetPrototype existingLayoutSetPrototype = null;
 
-			if (Validator.isNotNull(layoutSetPrototypeUuid)) {
+			if (Validator.isNotNull(importedLayoutSetPrototypeUuid)) {
 				try {
 					existingLayoutSetPrototype =
 						LayoutSetPrototypeLocalServiceUtil.
-							getLayoutSetPrototypeByUuid(layoutSetPrototypeUuid);
+							getLayoutSetPrototypeByUuid(importedLayoutSetPrototypeUuid);
 				}
-				catch(NoSuchLayoutSetPrototypeException nslspe) {
+				catch (NoSuchLayoutSetPrototypeException nslspe) {
 				}
 			}
 
 			if (existingLayoutSetPrototype == null) {
-				layoutSetPrototype.setUuid(layoutSetPrototypeUuid);
+				layoutSetPrototype.setUuid(importedLayoutSetPrototypeUuid);
 
 				LayoutSetPrototypeLocalServiceUtil.updateLayoutSetPrototype(
 					layoutSetPrototype);
 			}
 		}
-
-		String layoutSetPrototypeUuid = layoutsElement.attributeValue(
-			"layout-set-prototype-uuid");
+		else if (larType.equals("layout-set-prototype")) {
+			layoutSetPrototypeUuid = GetterUtil.getString(
+				headerElement.attributeValue("type-uuid"));
+		}
 
 		ServiceContext serviceContext =
 			ServiceContextThreadLocal.getServiceContext();
