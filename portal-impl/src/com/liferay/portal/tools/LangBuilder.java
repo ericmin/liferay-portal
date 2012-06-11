2292b947ddeb2cd865fa9a660c7c60bdc294d4b2
Brian Chan <brian.chan@liferay.com>
2012-06-11 14:01:48 -0700

    LPS-27790 Source formatting

--- portal-impl/src/com/liferay/portal/tools/LangBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/LangBuilder.java
@@ -618,7 +618,7 @@ public class LangBuilder {
 
 			if (cause instanceof MicrosoftTranslatorException) {
 				System.out.println(
-					cause.getClass().getName() + " - " + cause.getMessage());
+					cause.getClass().getName() + ": " + cause.getMessage());
 			}
 			else {
 				e.printStackTrace();
