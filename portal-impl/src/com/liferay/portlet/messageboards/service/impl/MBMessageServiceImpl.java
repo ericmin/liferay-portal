818204cc1603f23f64458296da0eef242498e580
Hugo Huijser <hugo.huijser@liferay.com>
2012-06-12 10:28:31 -0700

    LPS-27845 Source formatting

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBMessageServiceImpl.java
@@ -594,8 +594,8 @@ public class MBMessageServiceImpl extends MBMessageServiceBaseImpl {
 		boolean preview = ParamUtil.getBoolean(serviceContext, "preview");
 
 		if (preview &&
-			MBMessagePermission.contains(getPermissionChecker(),
-				message, ActionKeys.UPDATE)) {
+			MBMessagePermission.contains(
+				getPermissionChecker(), message, ActionKeys.UPDATE)) {
 
 			checkReplyToPermission(
 				message.getGroupId(), message.getCategoryId(),
